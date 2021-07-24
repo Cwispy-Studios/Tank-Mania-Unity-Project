1a86436d858bac2e7fd323dfbff29fd0a725390f
Tim Yeung <nsk2000@outlook.com>
2021-07-24 20:42:36 +0200

    V1 shockwave implementation
    Added shader for shockwave and implementation which manipulates these type of shaders in the scene when bullets are fired or hit

---
+++ Assets/Scripts/Visuals/ShockWaveHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CwispyStudios.TankMania.Visuals
+{
+  public class ShockWaveHandler : MonoBehaviour
+  {
+    [SerializeField] private Shader shockWaveShader;
+    [SerializeField] private float shootHeight;
+    [SerializeField] private float hitHeight;
+
+    private List<Material> shockWaveMaterials = new List<Material>();
+
+    private int positionId;
+    private int startId;
+    private int timeId;
+    private int heightId;
+
+    private void Awake()
+    {
+      foreach (var renderer in FindObjectsOfType<Renderer>())
+      {
+        if (renderer.material.shader == shockWaveShader)
+          shockWaveMaterials.Add(renderer.material);
+      }
+
+      BulletEvents.OnBulletFired += BulletFiredShockWave;
+      BulletEvents.OnBulletHit += BulletHitShockWave;
+
+      positionId = Shader.PropertyToID("_StartPos");
+      startId = Shader.PropertyToID("_StartShockWave");
+      timeId = Shader.PropertyToID("_CurrTime");
+      heightId = Shader.PropertyToID("_Height");
+    }
+
+    private void BulletFiredShockWave(Projectile.Projectile projectile)
+    {
+      StartShockWave(projectile, shootHeight);
+    }
+
+    private void BulletHitShockWave(Projectile.Projectile projectile)
+    {
+      StartShockWave(projectile, hitHeight);
+    }
+
+    private void StartShockWave(Projectile.Projectile projectile, float height)
+    {
+      Vector3 position = projectile.transform.position;
+
+      foreach (var material in shockWaveMaterials)
+      {
+        material.SetVector(positionId, position);
+        material.SetFloat(startId, 1);
+        material.SetFloat(timeId, Time.time);
+        material.SetFloat(heightId, height);
+      }
+    }
+  }
+}
