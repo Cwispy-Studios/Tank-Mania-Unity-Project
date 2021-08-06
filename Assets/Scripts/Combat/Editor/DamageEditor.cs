38f2851f97b454d21a8583f9adcf07cb3c592004
WZH94 <WZHao2679@hotmail.com>
2021-08-07 07:21:53 +0800

    Damage Editor properly saves changes of SplashDamage class

--- Assets/Scripts/Combat/Editor/DamageEditor.cs
+++ Assets/Scripts/Combat/Editor/DamageEditor.cs
@@ -6,79 +6,96 @@ namespace CwispyStudios.TankMania.Combat
   [CustomEditor(typeof(Damage))]
   public class DamageEditor : Editor
   {
+    SerializedProperty splashDamage;
+
+    SerializedProperty hasSplashDamage;
+    SerializedProperty splashRadius;
+    SerializedProperty splashDamagePercentage;
+    SerializedProperty hasSplashDamageRolloff;
+    SerializedProperty minRadiusPercentageRolloff;
+    SerializedProperty maxRadiusPercentageRolloff;
+    SerializedProperty minRadiusDamagePercentageRolloff;
+    SerializedProperty maxRadiusDamagePercentageRolloff;
+
+    private void OnEnable()
+    {
+      splashDamage = serializedObject.FindProperty(nameof(SplashDamage));
+
+      hasSplashDamage = splashDamage.FindPropertyRelative(nameof(SplashDamage.HasSplashDamage));
+      splashRadius = splashDamage.FindPropertyRelative(nameof(SplashDamage.SplashRadius));
+      splashDamagePercentage = splashDamage.FindPropertyRelative(nameof(SplashDamage.SplashDamagePercentage));
+      hasSplashDamageRolloff = splashDamage.FindPropertyRelative(nameof(SplashDamage.HasSplashDamageRolloff));
+      minRadiusPercentageRolloff = splashDamage.FindPropertyRelative(nameof(SplashDamage.MinRadiusPercentageRolloff));
+      maxRadiusPercentageRolloff = splashDamage.FindPropertyRelative(nameof(SplashDamage.MaxRadiusPercentageRolloff));
+      minRadiusDamagePercentageRolloff = splashDamage.FindPropertyRelative(nameof(SplashDamage.MinRadiusDamagePercentageRolloff));
+      maxRadiusDamagePercentageRolloff = splashDamage.FindPropertyRelative(nameof(SplashDamage.MaxRadiusDamagePercentageRolloff));
+    }
+
     public override void OnInspectorGUI()
     {
       DrawDefaultInspector();
 
-      Damage damage = (Damage)target;
-      SplashDamage splashDamage = damage.SplashDamage;
+      serializedObject.Update();
 
       EditorGUILayout.Space();
 
       EditorStyles.label.fontStyle = FontStyle.Bold;
-      splashDamage.HasSplashDamage = 
-        EditorGUILayout.Toggle("Splash Damage", splashDamage.HasSplashDamage);
+      hasSplashDamage.boolValue = EditorGUILayout.BeginToggleGroup("Splash Damage", hasSplashDamage.boolValue);
       EditorStyles.label.fontStyle = FontStyle.Normal;
 
-      if (splashDamage.HasSplashDamage)
-      {
-        EditorGUI.indentLevel = 1;
+      EditorGUI.indentLevel = 1;
 
-        splashDamage.SplashRadius = 
-          EditorGUILayout.Slider("Radius", splashDamage.SplashRadius, 0f, 50f);
-        splashDamage.SplashDamagePercentage = 
-          EditorGUILayout.Slider("Damage %", splashDamage.SplashDamagePercentage, 0f, 1f);
+      splashRadius.floatValue = EditorGUILayout.Slider("Radius", splashRadius.floatValue, 0f, 50f);
+      splashDamagePercentage.floatValue = EditorGUILayout.Slider("Damage %", splashDamagePercentage.floatValue, 0f, 1f);
 
-        EditorGUILayout.Space();
+      EditorGUILayout.Space();
 
-        splashDamage.HasSplashDamageRolloff = 
-          EditorGUILayout.Toggle(
-            new GUIContent("Damage Rolloff", "Allows damage to attenuate based on how close the projectile landed to the object."), 
-            splashDamage.HasSplashDamageRolloff
-            );
+      hasSplashDamageRolloff.boolValue = EditorGUILayout.BeginToggleGroup(
+          new GUIContent("Damage Rolloff", "Allows damage to attenuate based on how close the projectile landed to the object."),
+          hasSplashDamageRolloff.boolValue
+        );
 
-        if (splashDamage.HasSplashDamageRolloff)
-        {
-          EditorGUI.indentLevel = 2;
+      EditorGUILayout.EndToggleGroup();
 
-          EditorStyles.label.fontStyle = FontStyle.Bold;
-          EditorGUILayout.LabelField("Radius Cutoff");
-          EditorStyles.label.fontStyle = FontStyle.Normal;
+      EditorGUI.indentLevel = 2;
 
-          splashDamage.MinRadiusPercentageRolloff =
-            EditorGUILayout.Slider(
-              new GUIContent("Min Radius %", "Percentage distance from the original radius the rolloff starts."),
-              splashDamage.MinRadiusPercentageRolloff,
-              0f, splashDamage.MaxRadiusPercentageRolloff
-            );
+      EditorStyles.label.fontStyle = FontStyle.Bold;
+      EditorGUILayout.LabelField("Radius Cutoff");
+      EditorStyles.label.fontStyle = FontStyle.Normal;
 
-          splashDamage.MaxRadiusPercentageRolloff =
-            EditorGUILayout.Slider(
-              new GUIContent("Max Radius %", "Percentage distance from the original radius the rolloff ends."),
-              splashDamage.MaxRadiusPercentageRolloff,
-              splashDamage.MinRadiusPercentageRolloff, 1f);
+      minRadiusPercentageRolloff.floatValue = EditorGUILayout.Slider(
+          new GUIContent("Min Radius %", "Percentage distance from the original radius the rolloff starts."),
+          minRadiusPercentageRolloff.floatValue,
+          0f, maxRadiusPercentageRolloff.floatValue
+        );
 
-          EditorStyles.label.fontStyle = FontStyle.Bold;
-          EditorGUILayout.LabelField("Damage Percentages of Radius");
-          EditorStyles.label.fontStyle = FontStyle.Normal;
+      maxRadiusPercentageRolloff.floatValue = EditorGUILayout.Slider(
+          new GUIContent("Max Radius %", "Percentage distance from the original radius the rolloff ends."),
+          maxRadiusPercentageRolloff.floatValue,
+          minRadiusPercentageRolloff.floatValue, 1f
+        );
 
-          splashDamage.MinRadiusDamagePercentageRolloff =
-            EditorGUILayout.Slider(
-              new GUIContent("Min Damage %", "Percentage damage dealt where the rolloff starts."),
-              splashDamage.MinRadiusDamagePercentageRolloff,
-              0f, 1f
-              );
+      EditorStyles.label.fontStyle = FontStyle.Bold;
+      EditorGUILayout.LabelField("Damage Percentages of Radius");
+      EditorStyles.label.fontStyle = FontStyle.Normal;
 
-          splashDamage.MaxRadiusDamagePercentageRolloff =
-            EditorGUILayout.Slider(
-              new GUIContent("Max Damage %", "Percentage damage dealt where the rolloff ends."),
-              splashDamage.MaxRadiusDamagePercentageRolloff,
-              0f, 1f
-              );
+      minRadiusDamagePercentageRolloff.floatValue = EditorGUILayout.Slider(
+          new GUIContent("Min Damage %", "Percentage damage dealt where the rolloff starts."),
+          minRadiusDamagePercentageRolloff.floatValue,
+          0f, 1f
+        );
 
-          EditorGUI.indentLevel = 0;
-        }
-      }
+      maxRadiusDamagePercentageRolloff.floatValue = EditorGUILayout.Slider(
+          new GUIContent("Max Damage %", "Percentage damage dealt where the rolloff ends."),
+          maxRadiusDamagePercentageRolloff.floatValue,
+          0f, 1f
+        );
+
+      EditorGUI.indentLevel = 0;
+
+      EditorGUILayout.EndToggleGroup();
+
+      serializedObject.ApplyModifiedProperties();
     }
   }
 }
