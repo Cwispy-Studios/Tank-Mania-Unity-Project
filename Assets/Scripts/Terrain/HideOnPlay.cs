6e2886aa23eb4aea2049b287c8efcb7278c4739c
Zephrax <Z3PHR4X@users.noreply.github.com>
2021-09-16 19:28:46 +0200

    Removed Update() from HideOnPlay

--- Assets/Scripts/Terrain/HideOnPlay.cs
+++ Assets/Scripts/Terrain/HideOnPlay.cs
@@ -4,16 +4,9 @@ namespace CwispyStudios.TankMania.Terrain
 {
     public class HideOnPlay : MonoBehaviour
     {
-        // Start is called before the first frame update
         private void Start()
         {
             gameObject.SetActive(false);
-        }
-
-        // Update is called once per frame
-        private void Update()
-        {
-        
         }
     }
 }
