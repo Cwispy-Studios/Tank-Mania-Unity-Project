4a138a4a1ccef6d2d80852eb8308d4e3c86709ec
WZH94 <WZHao2679@hotmail.com>
2021-11-25 03:17:45 +0100

    Removed commented code and unused variable from TurretSlot

--- Assets/Scripts/Player/TurretSlot.cs
+++ Assets/Scripts/Player/TurretSlot.cs
@@ -5,21 +5,7 @@ namespace CwispyStudios.TankMania.Player
   public class TurretSlot : MonoBehaviour
   {
     [SerializeField] private TurretRotationLimits turretRotationLimits;
-    [SerializeField] private Vector3 turretRotation;
 
     public TurretRotationLimits RotationLimits => turretRotationLimits;
-
-    //private Turret occupiedTurret;
-    //public Turret OccupiedTurret => occupiedTurret;
-
-    //private bool isUnlocked = false;
-    //public bool IsUnlocked => isUnlocked;
-
-    //public bool IsOccupied => occupiedTurret != null;
-
-    //public void SetTurretOnSlot( Turret turret )
-    //{
-    //  occupiedTurret = turret;
-    //}
   }
 }
